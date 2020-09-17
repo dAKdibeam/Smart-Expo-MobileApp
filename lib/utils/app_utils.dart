2c3b585b62e5f8d213d441e60c497e78a6a4d3b3
dAKdibeam <kriskorn2u@gmail.com>
2020-09-17 20:55:09 +0700

    First commit

---
+++ lib/utils/app_utils.dart
@@ -0,0 +1,4 @@
+num offsetToOpacity(
+    {double currentOffset, double returnMax = 1, double maxOffset}) {
+  return (currentOffset * returnMax) / maxOffset;
+}
---
+++ lib/widgets/nearby_event_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:app1/constant/color.dart';
+import 'package:app1/constant/text_style.dart';
+import 'package:app1/models/event_model.dart';
+import 'package:app1/utils/datetime_utils.dart';
+import 'package:app1/widgets/ui_helper.dart';
+
+class NearbyEventCard extends StatelessWidget {
+  final Event event;
+  final Function onTap;
+  NearbyEventCard(this.event, {this.onTap});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16),
+      child: InkWell(
+        onTap: () {
+          onTap();
+        },
+        child: Row(
+          children: <Widget>[
+            buildImage(),
+            buildEventInfo(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildImage() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        color: imgBG,
+        width: 80,
+        height: 100,
+        child: Hero(
+          tag: event.image,
+          child: Image.network(
+            event.image,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildEventInfo(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(DateTimeUtils.getFullDate(event.eventDate), style: monthStyle),
+          UIHelper.verticalSpace(8),
+          Text(event.name, style: titleStyle),
+          UIHelper.verticalSpace(8),
+          Row(
+            children: <Widget>[
+              Icon(Icons.location_on,
+                  size: 16, color: Theme.of(context).primaryColor),
+              UIHelper.horizontalSpace(4),
+              Text(event.location.toUpperCase(), style: subtitleStyle),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
