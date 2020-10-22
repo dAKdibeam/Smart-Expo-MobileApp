cc1258269e9f5db806d6e3fce97b0a90e140ba64
dAKdibeam <[kriskorn2u@gmail.com]>
2020-10-22 18:00:22 +0700

    edit color

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -4,3 +4,4 @@ const Color lime = Color.fromRGBO(240, 203, 148, 1);
 const Color orange = Color.fromRGBO(255, 158, 107, 1);
 const Color primaryLight = Color.fromRGBO(240, 234, 248, 1);
 const Color imgBG = Color.fromRGBO(230, 230, 254, 1);
+const Color pastel = Color.fromRGBO(245, 158, 167, 1);
