e06b4a1e83704c46af15a7600e60d05d4fcb08a8
dAKdibeam <[kriskorn2u@gmail.com]>
2020-10-24 00:55:43 +0700

    image picker done

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,4 +1,7 @@
+import 'package:app1/constant/color.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class SignupPage extends StatefulWidget {
   @override
@@ -6,6 +9,56 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  File _image;
+
+  void _imgFromCamera() async {
+    File image = await ImagePicker.pickImage(
+        source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void _imgFromGallery() async {
+    File image = await ImagePicker.pickImage(
+        source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void _showPicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Photo Library'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -27,8 +80,44 @@ class _SignupPageState extends State<SignupPage> {
                   ],
                 ),
               ),
+              SizedBox(
+                height: 32,
+              ),
               Container(
-                  padding: EdgeInsets.only(top: 175.0, left: 20.0, right: 20.0),
+                padding: EdgeInsets.fromLTRB(140.0, 0.0, 0.0, 0.0),
+                child: GestureDetector(
+                  onTap: () {
+                    _showPicker(context);
+                  },
+                  child: CircleAvatar(
+                    radius: 55,
+                    backgroundColor: Color.fromRGBO(240, 203, 148, 1),
+                    child: _image != null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.file(
+                              _image,
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.fitHeight,
+                            ),
+                          )
+                        : Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(50)),
+                            width: 100,
+                            height: 100,
+                            child: Icon(
+                              Icons.camera_alt,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                  ),
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
                   child: Column(
                     children: <Widget>[
                       TextField(
